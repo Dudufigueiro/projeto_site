248f7275ff0533cfb00d8ae9655ef91982dfffab
Eduardo <edu_pzo03@hotmail.com>
2025-05-13 15:12:24 -0300

    Fix: Ajuste tela de login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -14,12 +14,12 @@ router.post('/login', async (req, res) => {
     if (result.rows.length === 0) return res.send('Usuário não encontrado.');
 
     const usuario = result.rows[0];
-   if (senha === usuario.senha) {
-  req.session.userId = usuario.id;
-  res.send('Login bem-sucedido!');
-} else {
-  res.send('Senha incorreta.');
-}
+    if (senha === usuario.senha) {
+      req.session.userId = usuario.id;
+      res.send('Login bem-sucedido!');
+    } else {
+      res.send('Senha incorreta.');
+    }
 
   } catch (err) {
     console.error(err);
